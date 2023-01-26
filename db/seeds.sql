f771d76d3e9fd13b1f692aafdb0a7f121940f03f
github_kevoconn <kcoconnor84@gmail.com>
2023-01-25 18:53:14 -0600

    added roles and employees in command line

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,14 +11,14 @@ VALUES
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Director', 100000, 1),
-    ('Chief Engineer', 80000, 1),
-    ('Engineer Foreman', 150000, 2),
-    ('Engineer', 120000, 2),
-    ('Electrician Foreman', 160000, 3),
-    ('Electrician', 125000, 3),
-    ('Plumbing Foreman', 250000, 4),
-    ('Plumber', 190000, 4);
+    ('Director', 200000, 1),
+    ('Chief Engineer', 115000, 1),
+    ('Engineer Foreman', 110000, 2),
+    ('Engineer', 105000, 2),
+    ('Electrician Foreman', 110000, 3),
+    ('Electrician', 105000, 3),
+    ('Plumbing Foreman', 110000, 4),
+    ('Plumber', 105000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
--- index.js
+++ index.js
@@ -1,41 +1,62 @@
 const inquirer = require("inquirer");
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const cTable = require("console.table");
+require("dotenv").config();
 
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'Admin',
-      // MySQL password
-      password: 'Admin',
-      database: 'classlist_db'
-    },
-    console.log(`Connected to the classlist_db database.`)
-  );
-  
-  // Query database
-  db.query('SELECT * FROM students', function (err, results) {
-    console.log(results);
-  });
-
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "Admin",
+    database: "tracker",
+  },
+  console.log(`Connected to the tracker database.`)
+);
 
 function viewDepartments() {
-    console.log('it worked')
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+  });
+  console.log("success");
+}
+
+function viewRoles() {
+  db.query("SELECT * FROM role", function (err, results) {
+    console.table(results);
+  });
+}
+
+function viewEmployees() {
+  db.query("SELECT * FROM employee", function (err, results) {
+    console.table(results);
+});
 }
 
 inquirer
   .prompt([
     {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'choice',
-      choices: [{name: 'View all departments', value: 'VIEW_DEPARTMENTS'}]
-         }])
-  .then((response) =>{
-    console.log(response.choice)
-    if (response.choice === 'VIEW_DEPARTMENTS') {
-        viewDepartments();
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        { name: "View all departments", value: "VIEW_DEPARTMENTS" },
+        { name: "View all roles", value: "VIEW_ROLES" },
+        { name: "View all employees", value: "VIEW_EMPLOYEES" },
+      ],
+    },
+  ])
 
+  .then((response) => {
+    console.log(response.choice);
+    if (response.choice === "VIEW_DEPARTMENTS") {
+      viewDepartments();
     }
- 
+    if (response.choice === "VIEW_ROLES") {
+      viewRoles();
+    }
+    if (response.choice === "VIEW_EMPLOYEES") {
+      viewEmployees();
+    }
   });
